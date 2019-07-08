9a894e920949d3eef9a9f37f6db26c6fbeceb333
lickey10 <lickey10@gmail.com>
2019-07-08 12:16:01 -0600

    first import

---
+++ 2PersonInteraction/Assets/Scripts/CharacterActionController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterActionController : MonoBehaviour {
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("j"))
+            anim.SetBool("Jab", true);
+        if (Input.GetKeyDown("u"))
+            anim.SetBool("Hikick", true);
+        if (Input.GetKeyDown("i"))
+            anim.SetBool("Spinkick", true);
+        if (Input.GetKeyDown("o"))
+            anim.SetBool("Rising_P", true);
+        //if (Input.GetKeyDown("j"))
+        //    anim.SetBool("Headspring", true);
+        //if (GUI.Button(new Rect(Screen.width - 190, 210, 100, 20), "Land"))
+        //    anim.SetBool("Land", true);
+        if (Input.GetKeyDown("k"))
+            anim.SetBool("ScrewK", true);
+        //if (GUI.Button(new Rect(Screen.width - 190, 270, 100, 20), "DamageDown"))
+        //    anim.SetBool("DamageDown", true);
+        if (Input.GetKeyDown("l"))
+            anim.SetBool("SAMK", true);
+        //if (GUI.Button(new Rect(Screen.width - 190, 330, 100, 20), "Run"))
+        //    anim.SetBool("Run", true);
+        //if (GUI.Button(new Rect(Screen.width - 190, 360, 100, 20), "Run_end"))
+        //    anim.SetBool("Run", false);
+    }
+}
---
+++ 2PersonInteraction/Assets/Scripts/DamageHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageHandler : MonoBehaviour {
+    public GameObject[] strikingObjects;//fist,foot, head, etc
+
+	// Use this for initialization
+	void Start () {
+	    //add collliders to each strikingObject and set isTrigger to true
+        //foreach(GameObject strikingObject in strikingObjects)
+        //{
+            //strikingObject.AddComponent<BoxCollider>().isTrigger = true;
+        //}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision hit)
+    {
+        float damage = 0;
+
+        if (hit.gameObject.tag == "Fighter" && hit.relativeVelocity.magnitude > 1f)
+        {
+            damage = hit.relativeVelocity.magnitude;
+            Debug.Log("Ouch!");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if(other.gameObject.tag == "Fighter")
+        //    SendMessage("ApplyDamage", 10, SendMessageOptions.DontRequireReceiver);
+    }
+}
